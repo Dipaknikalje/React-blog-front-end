ce0761b9c4c25bf4887f793b91a76d771e8f05ea
Dipaknikalje <dipaknikalje80@gmail.com>
2023-06-02 18:45:05 +0530

    first commit

--- src/Utility/Routes.js
+++ src/Utility/Routes.js
@@ -1,13 +1,3 @@
-// import React from "react";
-// import { Routes, Route } from "react-router-dom";
-// import Home from "../Pages/Home/Home";
-// import Bollywood from "../Pages/Bollywood";
-// import Technology from "../Pages/Technology";
-// import Hollywood from "../Pages/Hollywood";
-// import Fitness from "../Pages/Fitness";
-// import Food from "../Pages/Food";
-// import Article from "../Pages/Article";
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home/Home";
@@ -21,15 +11,6 @@ import Article from "../Pages/Article";
 function RouteComp() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Bollywood" element={<Bollywood />} />
-        <Route path="/Technology" element={<Technology />} />
-        <Route path="/Hollywood" element={<Hollywood />} />
-        <Route path="/Fitness" element={<Fitness />} />
-        <Route path="/Food" element={<Food />} />
-        <Route path="/article/:Id" element={<Article />} />
-      </Routes>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Bollywood" element={<Bollywood />} />
